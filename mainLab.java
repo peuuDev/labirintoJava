3d3aa3a9af1c7d87d2a4cf912950278d55988e85
Pedro Ramos <peuramos1@gmail.com>
2026-03-30 14:30:57 -0300

    gerador aleatorio do labirinto completo. verificaçoes pendentes...

--- mainLab.java
+++ mainLab.java
@@ -1,20 +1,23 @@
+import java.util.Random;
+
 public class mainLab {
     public static void main(String[] args) {
         String[][] labirinto = new String[10][12];
-
-        System.out.println("");
-        for(int i = 0; i < labirinto.length; i++){
-            for(int j = 0; j < labirinto.length; j++){
-                if (j == 0 || j == 9) {
-                    labirinto[i][j] = "|";
-                }
-            }
-        }
-
+        String[] carac = {" . " , " # "};
+        labirinto[0][1] = " P ";
+        labirinto[9][10] = " S ";
         
+        System.out.println("");
 
+        //Gerador do labirinto
         for(int i = 0; i < labirinto.length; i++){
-            for(int j = 0; j < labirinto.length; j++){
+            for(int j = 0; j < labirinto[i].length; j++){
+                if (j == 0 || j == 11) {
+                    labirinto[i][j] = "|";
+                }else if(labirinto[i][j] == null){
+                    Random random = new Random();
+                    labirinto[i][j] = carac[random.nextInt(carac.length)]; 
+                }
                 System.out.print(labirinto[i][j]);
             }
             System.out.println();
